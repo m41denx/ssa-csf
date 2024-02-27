56876aae43cd85b3aacc2197e21046c70c731094
M41den <38406005+m41denx@users.noreply.github.com>
2024-02-27 12:37:13 +0300

    favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,14 @@ export default function Document() {
     <Html lang="en">
         <Head>
             <title>СНО ВГУ</title>
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <body>
+        <Main/>
+        <NextScript/>
+        </body>
     </Html>
   );
 }
